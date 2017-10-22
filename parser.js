714759cc1942c634c28b3f3d2d92b1b1c612e60f
Shiv Prakash Anand <shivakp2111@gmail.com>
2017-10-22 22:56:39 +0530

    Add files via upload

---
+++ parser.js
@@ -0,0 +1,65 @@
+function parser(tokens) {
+
+  let current = 0;
+
+  function walk() {
+
+    var token = tokens[current];
+    if (token.type === 'number') {
+      current++;
+
+      return {
+        type: 'NumberLiteral',
+        value: token.value,
+      };
+    }
+
+    if (token.type === 'string') {
+      current++;
+
+      return {
+        type: 'StringLiteral',
+        value: token.value,
+      };
+    }
+
+    if (token.type === 'paren' && token.value === '(') {
+
+      token = tokens[++current];
+
+      var node = {
+        type: 'CallExpression',
+        name: token.value,
+        params: [],
+      };
+
+      token = tokens[++current];
+      
+       while (
+        (token.type !== 'paren') ||
+        (token.type === 'paren' && token.value !== ')')
+      ) {
+     
+        node.params.push(walk());
+        token = tokens[current];
+      }
+
+      current++;
+      return node;
+    }
+
+    throw new TypeError(token.type);
+  }
+
+  var ast = {
+
+    type: 'Program',
+    body: [],
+  };
+
+  while (current < tokens.length) {
+    ast.body.push(walk());
+  }
+
+  return ast;
+}
