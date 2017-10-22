15ce9502825e8c43129348e1a44603145ca8be8f
Shiv Prakash Anand <shivakp2111@gmail.com>
2017-10-22 22:59:14 +0530

    Add files via upload

---
+++ tokens.js
@@ -0,0 +1,82 @@
+
+function tokenizer(input) {
+
+  var current = 0;
+  var tokens = [];
+
+  while (current < input.length) {
+
+    var char = input[current];
+
+    if (char === '(') {
+
+      tokens.push({
+        type: 'paren',
+        value: '('
+      });
+
+      current++;
+      continue;
+    }
+
+    if (char === ')') {
+      tokens.push({
+        type: 'paren',
+        value: ')'
+      });
+      current++;
+      continue;
+    }
+
+    var WHITESPACE = /\s/;
+    if (WHITESPACE.test(char)) {
+      current++;
+      continue;
+    }
+
+    var NUMBERS = /[0-9]/;
+    if (NUMBERS.test(char)) {
+
+      var value = '';
+
+      while (NUMBERS.test(char)) {
+        value += char;
+        char = input[++current];
+      }
+
+      tokens.push({
+        type: 'number',
+        value: value
+      });
+
+      continue;
+    }
+
+    var LETTERS = /[a-z]/i;
+    if (LETTERS.test(char)) {
+      var value = '';
+
+      while (LETTERS.test(char)) {
+        value += char;
+        char = input[++current];
+      }
+
+      tokens.push({
+        type: 'name',
+        value: value
+      });
+
+      continue;
+    }
+
+    throw new TypeError('I dont know what this character is: ' + char);
+  }
+
+  return tokens;
+}
+
+var input = '(add 2 (subtract 4 2))';
+
+var tokens = tokenizer(input);
+
+console.log(tokens);
